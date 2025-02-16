f1cb18b372cd737816e6bda708a64e9577efad30
Patrick (The Website Guy) <patrick@thewebsiteguy.uk>
2025-02-16 07:59:50 +0000

    wip

--- plugins/beautyawakening/treatments/lang/en/lang.php
+++ plugins/beautyawakening/treatments/lang/en/lang.php
@@ -7,4 +7,6 @@
     'name' => 'Name',
     'manage_treatments' => 'Manage Treatments',
     'treatment' => 'Treatment',
+    'treatment_disclaimer' => 'Disclaimer',
+    'treatment_category_select' => 'Select treatment category',
 ];
